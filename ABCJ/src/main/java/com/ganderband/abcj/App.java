c110d13958d83c5bf17fc44f2a3646bd2320cbf7
Gwenael Lambrouin <gwenael.lambrouin@gmail.com>
2014-08-29 00:01:50 +0200

    Maven-generated skeleton.

---
+++ ABCJ/src/main/java/com/ganderband/abcj/App.java
@@ -0,0 +1,13 @@
+package com.ganderband.abcj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
